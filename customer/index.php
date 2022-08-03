4a5fb84c45fedb48b54ad6cf6aa724a27ecdb8d5
Shwe Zin <70483157+ShweZin-coder@users.noreply.github.com>
2022-08-03 23:10:32 +0630

    Add files via upload

---
+++ customer/index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nature Land </title>
+    <link rel="stylesheet" href="../assets/css/customer/style.css">
+    <link rel="icon" type="image/x-icon" href="../assets/img/admin/naturelandlogo.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+    <link rel="stylesheet" href="../assets/css/customer/style.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
+<body>
+ <!---Header Section Start-->   
+ <header class="header">
+    <div id="menu-btn" class="fa fa-bars icons"></div>
+    <div id="search-btn" class="fa fa-search icons"> </div>
+        <nav class="navbar">
+            <a href="#home">home</a>
+            <a href="#menu">menu</a>
+            <a href="#service">service</a>
+            <span class="space"></span>
+            <a href="#about">about</a>
+            <a href="#contact">contact</a>
+            <a href="signin.html">sign in</a>
+        </nav>
+      <a href="#" class="fas fa-shopping-cart icons"></a>
+      <a href="#home" class="logo">
+        <img src="../assets/img/admin/naturelandlogo.png" width="75px" height="50px">
+      </a>
+      <form action="" class="search-form">
+         <input type="search" name="" placeholder="Search here..." id="search-box">
+         <label for="search-box" class="fas fa-search icons"></label>
+      </form>
+ </header>
+ <!---Header Section End-->  
+ <!--Home Section Start-->
+ <section class="home" id="home">
+    <div class="content" >
+        <a href="#menu" class="btn" data-aos="fade-up">Our Menu</a>
+    </div>
+ </section>
+ <!---Home Section End-->
+ <!----Service Section Start-->
+ <section class="service" id="service">
+    <div class="box" data-aos="zoom-in">
+            <i class="fa fa-table"></i>
+            <h3>Table Services</h3>
+            <p>In this type of service, the guests enter the dining area and take seats. The waiter offers them water and menu card. The guests then place their order to the waiter. The table is covered in this service. It is grouped into the following types.</p>
+    </div>
+    <div class="box" data-aos="zoom-in">
+        <i class="fa fa-cloud-meatball"></i>
+        <h3>Buffett Services</h3>
+        <p>The guests get plates from the stack and goes to buffet counter where food is kept in large casseroles and platters with burners. The guests can serve themselves or can request the server behind the buffet table to serve. In sit-down buffet restaurants, the tables are arranged with crockery and cutlery where guests can sit and eat, and then replenish their plates. </p>
+    </div>
+    <div class="box" data-aos="zoom-in">
+        <i class="fa fa-user-tie"></i>
+        <h3>Self Services</h3>
+        <p> The guests enter the dining area and select food items. They pay for coupons of respective food items. They go to food counter and give the coupons to avail the chosen food. The guests are required to take their own plates to the table and eat. </p>
+    </div>
+ </section>
+ <!----Service Section End-->
+ <!---Menu Section Start-->
+ <section class="menu" id="menu">
+    <div class="heading" data-aos="fade-up">
+        <img src="../assets/img/customer/title-img.png">
+        <h3>Our menu</h3>
+    </div>
+    <div class="box-container">
+        <div class="box" data-aos="zoom-in">
+             <img src="../assets/img/customer/product-1.png" alt="hamburger">
+             <div class="content">
+                <div class="stars">
+                     <i class="fa fa-star"></i>
+                     <i class="fa fa-star"></i>
+                     <i class="fa fa-star"></i>
+                     <i class="fa fa-star"></i>
+                     <i class="fa fa-star-half-alt"></i>
+                </div>
+                <h3>Humberger</h3>
+                <div class="price">9000 MMK</div>
+                <a href="#" class="cartbtn">add to cart</a>
+             </div>
+        </div>
+        <div class="box" data-aos="zoom-in">
+            <img src="../assets/img/customer/product-2.png" alt="hamburger">
+            <div class="content">
+               <div class="stars">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half-alt"></i>
+               </div>
+               <h3>Pizza</h3>
+               <div class="price">16000 MMK</div>
+               <a href="#" class="cartbtn">add to cart</a>
+            </div>
+       </div>
+      <div class="box" data-aos="zoom-in">
+        <img src="../assets/img/customer/product-3.png" alt="hamburger">
+        <div class="content">
+           <div class="stars">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-half-alt"></i>
+           </div>
+           <h3>Mar Lar Xiang Guo</h3>
+           <div class="price">12000 MMK</div>
+           <a href="#" class="cartbtn">add to cart</a>
+        </div>
+   </div>
+   <div class="box" data-aos="zoom-in">
+    <img src="../assets/img/customer/product-4.png" alt="hamburger">
+    <div class="content">
+       <div class="stars">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-half-alt"></i>
+       </div>
+       <h3>Tofu</h3>
+       <div class="price">5500 MMK</div>
+       <a href="#" class="cartbtn">add to cart</a>
+    </div>
+</div>
+<div class="box" data-aos="zoom-in">
+    <img src="../assets/img/customer/product-5.png" alt="hamburger">
+    <div class="content">
+       <div class="stars">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-half-alt"></i>
+       </div>
+       <h3>Pork Steak</h3>
+       <div class="price">12000 MMK</div>
+       <a href="#" class="cartbtn">add to cart</a>
+    </div>
+</div>
+<div class="box" data-aos="zoom-in">
+    <img src="../assets/img/customer/product-6.png" alt="hamburger">
+    <div class="content">
+       <div class="stars">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-half-alt"></i>
+       </div>
+       <h3>Mar Lar Xiang Guo</h3>
+       <div class="price">6000 MMK</div>
+       <button class="cartbtn">add to cart</button>
+    </div>
+  </div>
+ </div>
+ <div class="heading">
+    <a href="" class="btn" style="margin:auto;">More Menu <i class="fa fa-arrow-right"></i></a>
+</div>
+
+</section>
+ <!---Menu Section End-->
+<!---About Section Start-->
+ <section class="about" id="about">
+    <div class="image" data-aos="zoom-in-right">
+        <img src="../assets/img/customer/about-img.jpg">
+    </div>
+    <div class="content" data-aos="zoom-in-left">
+        <h3 class="title">About Us</h3>
+        <p>
+            Rising high into the Yangon skyline, a paragon of stunning modern architecture, LOTTE HOTEL YANGON sets a new standard for 5-star 
+            accommodation in Myanmar and it stands in unparalleled, awe-inspiring surroundings. Our exceptional establishment offers an array 
+            of world-class facilities catering for every variety of guest. From LOTTE HOTEL YANGON’s premier suites, replete with sweeping views 
+            over Inya Lake, to our variety of fine-dining restaurants, and a banquet hall crafted to host standout international events and seminars, 
+            we are positive that our hospitality will inspire guests to discover their relaxation, business success, or a stay of refined elegance.
+        </p>
+        <div class="icons">
+            <h3><i class="fa fa-check"></i>Rooms</h3>
+            <h3><i class="fa fa-check"></i>Dining</h3>
+            <h3><i class="fa fa-check"></i>Wedding & Conference</h3>
+            <h3><i class="fa fa-check"></i>Facilities</h3>
+        </div>
+        <a href="#" class="btn">Read More</a>
+    </div>
+ </section>
+<!---About Section End-->
+<!-----------Contact Us Section Start --------------->
+<section class="contact" id="contact">
+    <div class="heading">
+        <img src="../assets/img/customer/title-img.png">
+        <h3>Contact Us</h3>
+    </div>
+    <div class="row">
+        <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d977549.4854916971!2d95.6967460383553!3d16.848533234023137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2smm!4v1659347403459!5m2!1sen!2smm" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" data-aos="zoom-in-right"></iframe>
+
+        <div class="form" data-aos="zoom-in-left">
+        <div class="icons-container">
+            <div class="icons">
+               <i class="fas fa-map"></i>
+               <h3>Address</h3>
+               <p>Yangon</p>
+            </div>
+            <div class="icons">
+                <i class="fas fa-envelope"></i>
+                <h3>email</h3>
+                <p>natureland@gmail.com</p>
+             </div>
+             <div class="icons">
+                <i class="fas fa-phone"></i>
+                <h3>phoneno</h3>
+                <p>09-5373210</p>
+             </div>
+            </div>
+             <form action="">
+                <input type="text" name="name" placeholder="full name" class="box">
+                <input type="email" name="email" placeholder="full email" class="box">
+                <input type="number" name="phoneno" placeholder="phoneno" class="box">
+                <textarea name="message" placeholder="message" class="box" cols="30" rows="10"></textarea>
+                <input type="submit" value="Send Message" class="box">
+            </form>
+    </div>
+    </div>
+</section>
+<!-----------Contact Us Section End   --------------->
+<!-----------Footer Section Start     --------------->
+<section class="footer" data-aos="fade-up">
+    <div class="heading">
+        <img src="../assets/img/admin/naturelandlogo.png">
+        <p>&copy;copyright:  2022 - 2026 <span style="margin:40px;font-size:3rem;" class="bar">| <br style="display:none;"></span> Developed by <span style="color:yellow;">Shwe Zin</span></p>
+    </div>
+</section>
+<!-----------Footer Section End       --------------->
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script src="../assets/js/customer/script.js"></script>
+</body>
+</html>
